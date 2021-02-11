5d7c6337e3fc35618ee956dcdd4c26536c3f6b04
534543 <70906918+534543@users.noreply.github.com>
2021-02-11 16:11:52 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -131,7 +131,50 @@ function setScrollType(){
 
 
 
+document.addEventListener("DOMContentLoaded",function (){
+    const form = document.getElementById("book_form");
+    form.addEventListener("submit", formSend);
 
+
+    async function formSend(e){
+        e.preventDefault();
+
+
+        let error = formValidate(form);
+
+    }
+
+    function formValidate(form){
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for(let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+
+            if(input.classList.contains('_email')){
+                if(emailTest(input)){
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+    }
+
+    function formAddError(input){
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input){
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error')
+    }
+
+    function emailTest(input){
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.value);
+    }
+})
 const animItems = document.querySelectorAll('._anim-items');
 
 if( animItems.length > 0){
@@ -155,6 +198,7 @@ if( animItems.length > 0){
             }else{
                 //animItem.classList.remove('_active');
             }
+            
         }
     }
     function offset(el) {
